d7562c0c241e0370551fd899a9b1a1559f07bfcd
Gregory Tang <gtang159@gmail.com>
2019-11-05 22:36:28 -0800

    Added border to bottom of banner and re-sizing profile section

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -18,6 +18,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-container">
+        <div className="about-text">About</div>
         <div className="profile-photo-container">
           <img
             src={this.state.imgURL}
