69b63ed23d2a14ccd684e77da5017b1bd749d7b9
Tangerinez <gtang159@gmail.com>
2019-11-06 16:05:22 -0800

    Fixed padding inconsistencies

--- portfolio/src/components/Bio/Bio.js
+++ portfolio/src/components/Bio/Bio.js
@@ -9,10 +9,10 @@ function Bio() {
         creating easy to use, intuitive UX/UI.
       </div>
       <div className="bio-content2">
-        Currently, I work as a software engineer in a startup in San Mateo
+        Currently, I work as a software engineer in San Mateo for a startup
         called Wavely. There, I work on the full frontend development process
         for many of our pages, which involves collaborating with designers and
-        product managers to create the best UX/UI.
+        product managers to create the best user experiences.
       </div>
       <div className="bio-content2">
         My interests lie strongly in using React.js and Vue.js along with HTML5,
