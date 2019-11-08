f315c25b2221bbbecac526decb6cc9877c5908f5
Gregory Tang <gtang159@gmail.com>
2019-11-07 23:34:17 -0800

    Nearly completed projects section

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -8,14 +8,68 @@ function Projects() {
       <div className="projects-text">Projects</div>
       <div className="flex-line-white"></div>
       <div className="card-container">
-        <ProjectCard imgURL="./wavely-about-page.png" alt="wavely-about-page" />
-        <ProjectCard imgURL="./locals.png" alt="locals" />
-        <ProjectCard imgURL="./tranquil.png" alt="tranquil" />
-        <ProjectCard imgURL="./react-chatroom.png" alt="react-chatroom" />
-        <ProjectCard imgURL="./clicky-game.png" alt="clicky-game" />
-        <ProjectCard imgURL="./swolemates.png" alt="swolemates" />
-        <ProjectCard imgURL="./friend-finder.png" alt="friend-finder" />
-        <ProjectCard imgURL="./shopping-cart.png" alt="shopping-cart" />
+        <ProjectCard
+          imgURL="./wavely-about-page.png"
+          alt="wavely-about-page"
+          cardTitle="Wavely About Page"
+          description="React + HTML5 + CSS3 + Node.js"
+          sourceURL="https://github.com/gregtang1234?tab=repositories"
+          appURL="https://about.wavely.com/"
+        />
+        <ProjectCard
+          imgURL="./locals.png"
+          alt="locals"
+          cardTitle="Locals"
+          description="React.js + HTML + CSS + Node.js/Express.js + MongoDB"
+          sourceURL="https://github.com/Tangerinez/Locals"
+          appURL="http://fathomless-bastion-86766.herokuapp.com"
+        />
+        <ProjectCard
+          imgURL="./react-chatroom.png"
+          alt="react-chatroom"
+          cardTitle="React Chatroom"
+          description="React.js + HTML + CSS + Node.js + Pusher ChatKit API"
+          sourceURL="https://github.com/Tangerinez/React-Chat-App"
+        />
+        <ProjectCard
+          imgURL="./swolemates.png"
+          alt="swolemates"
+          cardTitle="Swolemates"
+          description="Vanilla.js/JQuery + HTML + CSS/Bootstrap + Firestore"
+          sourceURL="https://github.com/Tangerinez/Swolemates.io"
+          appURL="https://tangerinez.github.io/Swolemates.io/"
+        />
+        <ProjectCard
+          imgURL="./tranquil.png"
+          alt="tranquil"
+          cardTitle="Tranquil"
+          description="Vanilla.js + HTML + CSS/Materialize + Node.js/Express.js + SQL"
+          sourceURL="https://github.com/Tangerinez/TranquiL"
+          appURL="https://rugged-gunnison-25361.herokuapp.com/"
+        />
+        <ProjectCard
+          imgURL="./clicky-game.png"
+          alt="clicky-game"
+          cardTitle="Clicky Game"
+          description="React.js + HTML + CSS/Bootstrap"
+          sourceURL="https://github.com/Tangerinez/ReactMemoryGame"
+        />
+        <ProjectCard
+          imgURL="./friend-finder.png"
+          alt="friend-finder"
+          id="friend-finder"
+          cardTitle="Friend Finder"
+          description="Vanilla.js + HTML + CSS + Node.js/Express.js"
+          sourceURL="https://github.com/Tangerinez/FriendFinder"
+        />
+        <ProjectCard
+          imgURL="./shopping-cart.png"
+          alt="shopping-cart"
+          id="shopping-cart"
+          cardTitle="Vue Shopping Cart"
+          description="Vue.js + HTML + CSS + Node.js"
+          sourceURL="https://github.com/Tangerinez/Vue-Shopping-Cart"
+        />
       </div>
     </div>
   );
